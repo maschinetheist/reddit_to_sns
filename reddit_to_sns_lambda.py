7a1547d0627f50a4e8db1f50da1d61e784cfd474
Mike <mike@project6209.com>
2017-04-20 19:38:45 -0500

    initial checkin

---
+++ reddit_to_sns_lambda.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import boto3
+import praw
+import logging
+from datetime import datetime, timedelta
+from dateutil import tz
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def reddit_to_sns(event, context):
+    '''
+    Parse subreddits for new submissions matching the search string and then
+    Send an AWS SNS message. This lambda function is best matched with a CloudWatch
+    Event Rule trigger set to rate(1 hour).
+
+    :param event:       pass lambda event parameter data that consists of:
+                        - awsregion
+                        - telephone number
+                        - subreddits
+                        - search string
+                        - reddit API client_id
+                        - reddit API client secret
+    :param context:     runtime information of lambda function
+    :return:            list of found submissions
+    :rtype:             list
+    '''
+    awsregion = event['awsregion']
+    tel_number = event['tel_number']
+    subreddits = event['subreddits']
+    searchstring = event['search_string']
+    reddit_client_id = event['client_id']
+    reddit_cilent_secret = event['client_secret']
+
+    list_of_subreddits = []
+    found_threads = []
+
+    # Set "last hour" object
+    last_hour = datetime.utcnow() - timedelta(hours=1)
+
+    # Instantiate Reddit objects
+    r = praw.Reddit(client_id='',
+                    client_secret='',
+                    user_agent='reddit_to_sns_get_posts')
+    sns = boto3.client('sns', region_name=awsregion)
+
+    if isinstance(subreddits, list) is False:
+        list_of_subreddits.append(subreddits)
+    else:
+        list_of_subreddits = subreddits
+
+    for sub in list_of_subreddits:
+        sub = sub.encode('ascii', 'ignore')
+        for thread in r.subreddit(sub).new(limit=15):
+            logger.info("Found thread containing the search string:", str(thread.title).lower())
+            if string in str(thread.title).lower():
+                created_time = datetime.utcfromtimestamp(thread.created_utc)
+                if created_time > last_hour:
+                    logger.info("Found a new Reddit thread")
+                    message = "New Reddit thread has been posted: " + str(thread.title)
+                    sns.publish(PhoneNumber=tel_number, Message=message)
+                    found_threads.append(thread.id)
+
+    if len(found_threads) >= 1:
+        return found_threads
